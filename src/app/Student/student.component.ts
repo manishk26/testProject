470d114c85aaa5b64eaff559d82e8afd9c25b09d
manishk26 <manish_m160043ca@nitc.ac.in>
2018-06-18 17:03:55 +0530

    error corrction in student

--- src/app/Student/student.component.ts
+++ src/app/Student/student.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core'
 @Component({
 	selector:'my-tag',
-	templateUrl:`app/Student/student.component.html`
+	templateUrl:`app/Student/student.component.html`,
 	styleUrls: ['app/Student/student.component.css']
 }) 
 export class StudentComponent{
